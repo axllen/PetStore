a4e13dba9a669217f74e570454e75744eaf0fbaa
chg <1021904353@qq.com>
2019-08-28 16:53:33 +0800

    190828

---
+++ ui/login_frame.py
@@ -0,0 +1,61 @@
+import wx
+
+
+class LoginFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='登录', size=(340, 230))
+        self.Center()
+        self.panel = wx.Panel(self)
+        self.icon = wx.Icon('resources\\icon\\dog4.ico', wx.BITMAP_TYPE_ICO)
+        self.SetIcon(self.icon)
+
+        # 用户名密码输入界面
+        user_text = wx.StaticText(self.panel, label='用户名：')
+        password_text = wx.StaticText(self.panel, label='密码：')
+        user_tc = wx.TextCtrl(self.panel)
+        password_tc = wx.TextCtrl(self.panel, style=wx.TE_PASSWORD)
+        login_grid = wx.FlexGridSizer(2, 2, 2)
+        login_grid.AddMany([
+            user_text,
+            (user_tc, 1, wx.EXPAND),
+            password_text,
+            (password_tc, 1, wx.EXPAND)
+        ])
+        login_grid.AddGrowableCol(0, 1)
+        login_grid.AddGrowableCol(1, 4)
+        login_grid.AddGrowableRow(0, 1)
+        login_grid.AddGrowableRow(1, 1)
+
+        # 登录按钮
+        submit_button = wx.Button(parent=self.panel, label='确定')
+        self.Bind(wx.EVT_BUTTON, self.submit_button_onclick, submit_button)
+        cancel_button = wx.Button(parent=self.panel, label='取消')
+        self.Bind(wx.EVT_BUTTON, self.cancel_button_onclick, cancel_button)
+        horizontal_box = wx.BoxSizer()
+        horizontal_box.Add(submit_button, 1, wx.CENTER | wx.ALL | wx.EXPAND, border=10)
+        horizontal_box.Add(cancel_button, 1, wx.CENTER | wx.ALL | wx.EXPAND, border=10)
+
+        # 垂直布局界面
+        vertical_box = wx.BoxSizer(wx.VERTICAL)
+        vertical_box.Add(login_grid, 1, wx.CENTER | wx.ALL | wx.EXPAND, 25)
+        vertical_box.Add(horizontal_box, 1, wx.CENTER | wx.BOTTOM, 20)
+
+        self.panel.SetSizer(vertical_box)
+
+    def submit_button_onclick(self, event):
+        pass
+
+    def cancel_button_onclick(self, event):
+        self.Destroy()
+
+
+class App(wx.App):
+    def OnInit(self):
+        login_frame = LoginFrame()
+        login_frame.Show()
+        return True
+
+
+if __name__ == '__main__':
+    app = App()
+    app.MainLoop()
---
+++ ui/product_list_frame.py
@@ -0,0 +1,9 @@
+import wx
+
+
+class ProductListFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(None, '请选择商品', (800, 600))
+        self.Center()
+        self.panel = wx.Panel(self)
+
